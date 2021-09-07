7c4466c2c2c6877879a828331d37493cb27849f2
Xgamer911 <Xgamer911@hotmail.com>
2021-09-07 18:07:11 -0500

    Lesson8-Complete

---
+++ IdleClickerGame/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public GameObject[] enemyPrefabs;
+    public Enemy curEnemy;
+    public Transform canvas;
+
+    public static EnemyManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void CreateNewEnemy ()
+    {
+        GameObject enemyToSpawn = enemyPrefabs[Random.Range(0, enemyPrefabs.Length)];
+
+        GameObject obj = Instantiate(enemyToSpawn, canvas);
+
+        curEnemy = obj.GetComponent<Enemy>();
+
+    }
+}
