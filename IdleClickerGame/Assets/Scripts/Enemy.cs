a8d52a994ba806b6bdeb6843d8d96522c25eb51f
Xgamer911 <Xgamer911@hotmail.com>
2021-09-07 19:09:42 -0500

    Lesson13-Complete

--- IdleClickerGame/Assets/Scripts/Enemy.cs
+++ IdleClickerGame/Assets/Scripts/Enemy.cs
@@ -9,12 +9,16 @@ public class Enemy : MonoBehaviour
     public int maxHp;
     public int goldToGive;
     public Image healthBarFill;
+    public Animation anim;
 
     public void Damage ()
     {
         curHP--;
 
         healthBarFill.fillAmount = (float)curHP / (float)maxHp;
+
+        anim.Stop();
+        anim.Play();
 
         if (curHP <= 0)
         {
